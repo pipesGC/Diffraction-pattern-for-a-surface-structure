1c2d7c7989c25a4cb4cde86c60224f9363f17ca0
Greis Cipi <greis.cipi@studio.unibo.it>
2023-09-11 10:45:50 +0200

    added comments for each function

--- create_cubic_structure.py
+++ create_cubic_structure.py
@@ -13,7 +13,21 @@ a_string = config.get('lattice_parameter', 'a')
 a = float(a_string)
 
 # Create a 3D grid of atoms for the specified cubic structure
-def generate_cubic_structure(structure, n):
+def generate_cubic_structure(
+                        structure : str,
+                        n : int
+):
+    """
+    Notes
+    -----
+    This function calls the right function to generate the cubic structure specified in config
+
+    Parameters
+    ----------
+    structure (str) : type of cubic structure (sc, bcc or fcc)
+    n (int) : number of repetitions of the structure to display along each axis
+
+    """
     if structure == "sc":
         return generate_simple_cubic(n)
     elif structure == "bcc":
@@ -24,16 +38,47 @@ def generate_cubic_structure(structure, n):
         raise ValueError("Invalid cubic_structure specified in config.ini")
 
 # Create a 3D grid of atoms for the simple cubic structure
-def generate_simple_cubic(n):
+def generate_simple_cubic(
+                        n : int
+) -> np.ndarray:
+    """
+    Notes
+    -----
+    This function calculates the atomic positions of a simple cubic structure
+
+    Parameters
+    ----------
+    n (int) : number of repetions of the cubic structure along each axis
+
+    Returns
+    -------
+    atomic_positions (np.ndarray) : 3-dim array cointaining the atomic positions
+    """
     atomic_positions = []
     for i in range(n):
         for j in range(n):
             for k in range(n):
                 atomic_positions.append([i * a, j * a, k * a])
-    return np.array(atomic_positions)
+    atomic_positions = np.array(atomic_positions)
+    return atomic_positions
 
 # Create a 3D grid of atoms for the body-centered cubic structure
-def generate_body_centered_cubic(n):
+def generate_body_centered_cubic(
+                                n : int
+) -> np.ndarray:
+    """
+    Notes
+    -----
+    This function calculates the atomic positions of a body centered cubic structure
+
+    Parameters
+    ----------
+    n (int) : number of repetions of the cubic structure along each axis
+
+    Returns
+    -------
+    atomic_positions (np.array) : 3-dim array cointaining the atomic positions
+    """
     atomic_positions = []
     for i in range(n):
         for j in range(n):
@@ -45,7 +90,22 @@ def generate_body_centered_cubic(n):
     return atomic_positions
 
 # Create a 3D grid of atoms for the face-centered cubic structure
-def generate_face_centered_cubic(n):
+def generate_face_centered_cubic(
+                                n : int
+) -> np.ndarray:
+    """
+    Notes
+    -----
+    This function calculates the atomic positions of a face centered cubic structure
+
+    Parameters
+    ----------
+    n (int) : number of repetions of the cubic structure along each axis
+
+    Returns
+    -------
+    atomic_positions (np.ndarray) : 3-dim array cointaining the atomic positions
+    """
     atomic_positions = []
     for i in range(n):
         for j in range(n):
@@ -54,7 +114,8 @@ def generate_face_centered_cubic(n):
                 atomic_positions.append([i * a + a/2, j * a + a/2, k * a])
                 atomic_positions.append([i * a + a/2, j * a, k * a + a/2])
                 atomic_positions.append([i * a, j * a + a/2, k * a + a/2])
-    return np.array(atomic_positions)
+    atomic_positions = np.array(atomic_positions)
+    return atomic_positions
 
 # Generate the specified cubic structure
 cubic_positions = generate_cubic_structure(cubic_structure, n)
