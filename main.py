b13efe1ce8068f9291fd3dc8dd432b91a9968eb6
Greis Cipi <greis.cipi@studio.unibo.it>
2023-09-12 16:56:42 +0200

    raise error and stop the run process when Nx, Ny or Nz is zero

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import subprocess
 import configparser
+import sys
 
 # Read configuration from the 'config.ini' file
 config = configparser.ConfigParser()
@@ -13,13 +14,14 @@ Nz = int(config.get('repetitions', 'Nz'))
 # Check if at least one of Nx, Ny, or Nz is zero
 if Nx == 0 or Ny == 0 or Nz == 0:
     print("Error: At least one of Nx, Ny, or Nz is equal to zero. Program will not proceed.")
-else:
-    # Run create_cubic_structure.py to generate the cubic structure
-    print("Generating the cubic structure...")
-    subprocess.run(["python", "create_cubic_structure.py"])
+    sys.exit(1)  # Raise an error and stop the program
 
-    # Run plot_cubic_structure.py to plot the cubic structure
-    print("Plotting the cubic structure...")
-    subprocess.run(["python", "plot_cubic_structure.py"])
+# Run create_cubic_structure.py to generate the cubic structure
+print("Generating the cubic structure...")
+subprocess.run(["python", "create_cubic_structure.py"])
 
-    print("Done.")
+# Run plot_cubic_structure.py to plot the cubic structure
+print("Plotting the cubic structure...")
+subprocess.run(["python", "plot_cubic_structure.py"])
+
+print("Done.")
