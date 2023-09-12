7bcf66b0cf4f4d229af720796b3075a35fd7220b
Greis Cipi <greis.cipi@studio.unibo.it>
2023-09-12 16:34:27 +0200

    first check if Nx, Ny and are different from zero to run the code (to be improved)

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import subprocess
+import configparser
 
-# Run create_cubic_structure.py to generate the cubic structure
-print("Generating the cubic structure...")
-subprocess.run(["python", "create_cubic_structure.py"])
+# Read configuration from the 'config.ini' file
+config = configparser.ConfigParser()
+config.read('config.ini')
 
-# Run plot_cubic_structure.py to plot the cubic structure
-print("Plotting the cubic structure...")
-subprocess.run(["python", "plot_cubic_structure.py"])
+# Extract repetition parameters from configuration
+Nx = int(config.get('repetitions', 'Nx'))
+Ny = int(config.get('repetitions', 'Ny'))
+Nz = int(config.get('repetitions', 'Nz'))
 
-print("Done.")
+# Check if at least one of Nx, Ny, or Nz is zero
+if Nx == 0 or Ny == 0 or Nz == 0:
+    print("Error: At least one of Nx, Ny, or Nz is equal to zero. Program will not proceed.")
+else:
+    # Run create_cubic_structure.py to generate the cubic structure
+    print("Generating the cubic structure...")
+    subprocess.run(["python", "create_cubic_structure.py"])
+
+    # Run plot_cubic_structure.py to plot the cubic structure
+    print("Plotting the cubic structure...")
+    subprocess.run(["python", "plot_cubic_structure.py"])
+
+    print("Done.")
