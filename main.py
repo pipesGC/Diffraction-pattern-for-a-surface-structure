681231981bd7d83c81142726caed1b41c40f71c6
Greis Cipi <greis.cipi@studio.unibo.it>
2023-09-12 17:06:11 +0200

    raising ValueError with try-except if Nx, Ny or Nz is equal to zero

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 import subprocess
 import configparser
-import sys
 
 # Read configuration from the 'config.ini' file
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-# Extract repetition parameters from configuration
-Nx = int(config.get('repetitions', 'Nx'))
-Ny = int(config.get('repetitions', 'Ny'))
-Nz = int(config.get('repetitions', 'Nz'))
-
-# Check if at least one of Nx, Ny, or Nz is zero
-if Nx == 0 or Ny == 0 or Nz == 0:
-    print("Error: At least one of Nx, Ny, or Nz is equal to zero. Program will not proceed.")
-    sys.exit(1)  # Raise an error and stop the program
-
 # Run create_cubic_structure.py to generate the cubic structure
-print("Generating the cubic structure...")
-subprocess.run(["python", "create_cubic_structure.py"])
+try:
+    print("Generating the cubic structure...")
+    create_structure_result = subprocess.run(["python", "create_cubic_structure.py"])
 
-# Run plot_cubic_structure.py to plot the cubic structure
-print("Plotting the cubic structure...")
-subprocess.run(["python", "plot_cubic_structure.py"])
+    # Check the return code of create_cubic_structure.py
+    if create_structure_result.returncode != 0:
+        raise ValueError("Error occurred in create_cubic_structure.py")
 
-print("Done.")
+    # Run plot_cubic_structure.py to plot the cubic structure
+    print("Plotting the cubic structure...")
+    subprocess.run(["python", "plot_cubic_structure.py"])
+
+    print("Done.")
+except subprocess.CalledProcessError as e:
+    print(f"Error: Subprocess error occurred - {e}")
+except ValueError as e:
+    print(f"Error: {e}")
