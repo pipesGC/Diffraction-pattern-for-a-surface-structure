2f22ffe25b009d459f032ea1265febd3fd716cfa
Greis Cipi <greis.cipi@studio.unibo.it>
2023-09-12 17:40:46 +0200

    added FileNotFoundError when it's not possible to find the cubic_structure_positions.txt file containing the atomic coordinates

--- plot_cubic_structure.py
+++ plot_cubic_structure.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-import matplotlib.figure
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import configparser
@@ -18,43 +17,46 @@ a = float(a_string)
 # Extract cubic structure
 cubic_structure = config.get('cubic_structure', 'type').lower()
 
+# Check if the 'cubic_structure_positions.txt' file exists
+if not os.path.isfile('cubic_structure_positions.txt'):
+    raise FileNotFoundError("Error: 'cubic_structure_positions.txt' file not found. Run create_cubic_structure.py first.")
+
 # Read atomic positions from the file
 cubic_positions = np.loadtxt('cubic_structure_positions.txt')
-cubic_positions /= a # renormalize the cubic structure to the lattice parameter 
+cubic_positions /= a  # renormalize the cubic structure to the lattice parameter
 
-# Plot a cubic structure
-def plot_cubic_structure(
-                    atomic_positions : np.ndarray,
-                    title : str = " "
-) -> matplotlib.figure.Figure:
+# Initialize variables to store selected atom indices and their colors
+atom_colors = ['r'] * len(cubic_positions)  # Initialize all atoms as red
+
+def plot_cubic_structure():
     """
-    Notes
-    -----
-    This function plots the crystal structure based on the given atomic positions 
-
-    Parameters
-    ----------
-    atomic_positions (np.ndarray) : 3-dim array containing the atomic positions
-    title (str) : title of the plot
-
-    Returns
-    -------
-    fig (matplotlib.figure.Figure): figure containing the plot
-
+    Create a 3D plot of the cubic structure.
     """
+    # Create a 3D plot
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(atomic_positions[:, 0], atomic_positions[:, 1], atomic_positions[:, 2], c='r', marker='o', s=100)
+
+    # Scatter plot of atom positions with colors
+    sc = ax.scatter(cubic_positions[:, 0], cubic_positions[:, 1], cubic_positions[:, 2], c=atom_colors, marker='o', s=100)
+
+    # Set axis labels
     ax.set_xlabel('a (au)')
     ax.set_ylabel('b (au)')
     ax.set_zlabel('c (au)')
-    ax.set_xlim(0, np.max(atomic_positions[:, 0]))
-    ax.set_ylim(0, np.max(atomic_positions[:, 1]))
-    ax.set_zlim(0, np.max(atomic_positions[:, 2]))
-    plt.title(title)
-    plt.show()
-    return fig
 
-# Add the element symbol to the plot title
-plot_title = f'{element_symbol} {cubic_structure} lattice with a = {a} Å'
-plot_cubic_structure(cubic_positions, plot_title)
+    # Set plot limits
+    ax.set_xlim(0, np.max(cubic_positions[:, 0]))
+    ax.set_ylim(0, np.max(cubic_positions[:, 1]))
+    ax.set_zlim(0, np.max(cubic_positions[:, 2]))
+
+    # Title with element symbol
+    plot_title = f'{element_symbol} {cubic_structure} lattice with a = {a} Å'
+    plt.title(plot_title)
+
+    # Display the plot
+    plt.show()
+
+
+# Call the functions
+plot_cubic_structure()
+
