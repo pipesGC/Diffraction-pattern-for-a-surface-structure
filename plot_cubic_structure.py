ee8fa78485fc5ac5354bf2e9e0c7d5ad280c45d5
Greis Cipi <greis.cipi@studio.unibo.it>
2023-09-18 11:17:44 +0200

    added function to save the plot as an image

--- plot_cubic_structure.py
+++ plot_cubic_structure.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import configparser
+import os
 
 # Read configuration from the 'config.ini' file
 config = configparser.ConfigParser()
@@ -16,6 +17,14 @@ a = float(a_string)
 
 # Extract cubic structure
 cubic_structure = config.get('cubic_structure', 'type').lower()
+
+# Extract the number of repetitions along each axis
+Nx_string = config.get('repetitions', 'Nx')
+Ny_string = config.get('repetitions', 'Ny')
+Nz_string = config.get('repetitions', 'Nz')
+Nx = int(Nx_string)
+Ny = int(Ny_string)
+Nz = int(Nz_string)
 
 # Check if the 'cubic_structure_positions.txt' file exists
 if not os.path.isfile('cubic_structure_positions.txt'):
@@ -56,7 +65,42 @@ def plot_cubic_structure():
     # Display the plot
     plt.show()
 
+def save_cubic_structure_plot(filename: str):
+    """
+    Save the cubic structure plot to a file.
+
+    Parameters:
+    ----------
+    filename (str): The name of the file to save the plot as.
+    """
+    # Create a 3D plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Scatter plot of atom positions with colors
+    sc = ax.scatter(cubic_positions[:, 0], cubic_positions[:, 1], cubic_positions[:, 2], c=atom_colors, marker='o', s=100)
+
+    # Set axis labels
+    ax.set_xlabel('a (au)')
+    ax.set_ylabel('b (au)')
+    ax.set_zlabel('c (au)')
+
+    # Set plot limits
+    ax.set_xlim(0, np.max(cubic_positions[:, 0]))
+    ax.set_ylim(0, np.max(cubic_positions[:, 1]))
+    ax.set_zlim(0, np.max(cubic_positions[:, 2]))
+
+    # Title with element symbol
+    plot_title = f'{element_symbol} {cubic_structure} lattice with a = {a} Å'
+    plt.title(plot_title)
+
+    # Save the plot as an image file
+    plt.savefig(filename)
+
+# Call the functions
+save_cubic_structure_plot('cubic_structure_plot.png')
 
 # Call the functions
 plot_cubic_structure()
-
+filename = f'{element_symbol}_{cubic_structure}_a{a}__Nx{Nx}_Ny{Ny}_Nz{Nz}'
+save_cubic_structure_plot(filename)
