222b3248fb3873780399de9f5ad2a15d21d664b6
Greis Cipi <greis.cipi@studio.unibo.it>
2023-09-19 19:25:54 +0200

    added test function that checks the right number of atoms for sc lattice

--- testing.py
+++ testing.py
@@ -16,3 +16,19 @@ def test_generate_simple_cubic(Nx, Ny, Nz):
     #Nx, Ny, Nz = 2,2,2
     atomic_positions = generate_simple_cubic(Nx, Ny, Nz)
     assert isinstance(atomic_positions, np.ndarray)
+
+
+# Test the generate_face_centered_cubic function
+@given(Nx=st.integers(min_value=1, max_value=20), Ny=st.integers(min_value=1, max_value=20), Nz=st.integers(min_value=1, max_value=20))
+@settings(deadline=400)  # Increase the deadline to 400ms
+def test_count_atoms_sc(Nx, Ny, Nz):
+    # Generate atomic positions for FCC
+    atomic_positions = generate_simple_cubic(Nx, Ny, Nz)
+    
+    # Calculate the expected number of atomic coordinates for SC
+    expected_count = (Nx + 1)*(Ny + 1)*(Nz + 1)
+    
+    # Check if the number of coordinates matches the expected count
+    
+    assert len(atomic_positions) == expected_count  
+    
