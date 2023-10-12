23c8e1529cafe6add35b8685fdab8bba5c5c22c8
Greis Cipi <greis.cipi@studio.unibo.it>
2023-10-12 15:49:24 +0200

    added testing function for the surface fcc(111) atomic coordinates generation

--- testing.py
+++ testing.py
@@ -1,7 +1,7 @@
 import numpy as np
 from hypothesis import given, settings
 from hypothesis import strategies as st
-from create_cubic_structure import generate_cubic_structure, generate_simple_cubic, generate_body_centered_cubic, generate_face_centered_cubic
+from create_cubic_structure import generate_cubic_structure, generate_simple_cubic, generate_body_centered_cubic, generate_face_centered_cubic, generate_111_surface_fcc
 
 
 # generating the variables Nx, Ny and Nz such that they are greater than 0
@@ -57,4 +57,18 @@ def test_count_atoms_fcc(Nx, Ny, Nz):
 
     # Check if the number of the coordinates matches the expected count
     assert len(atomic_positions) == expected_count
+
+# Test the generate_111_surface_fcc function
+@given(Na=st.integers(min_value=1, max_value=20), Nb=st.integers(min_value=1, max_value=20))
+def test_count_111_surface_fcc(Na, Nb):
+    # Generate atomic position for the surface fcc(111)
+    atomic_positions = generate_111_surface_fcc(Na, Nb)       
+
+    # Calculate the expected number of surface atomic cordinates for fcc(111)
+    expected_count = (Na + 1)*(Nb + 1)
+
+    # Check if the number of the coordinates matches the expected count
+    assert len(atomic_positions) == expected_count
+
+
 
